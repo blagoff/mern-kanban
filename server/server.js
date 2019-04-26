917c855e7c834c640f95319626a1038998042e5d
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-04-26 18:56:28 +0200

    API

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import IntlWrapper from '../client/modules/Intl/IntlWrapper';
 
+
 // Initialize the Express App
 const app = new Express();
 
@@ -45,6 +46,8 @@ import Helmet from 'react-helmet';
 // Import required modules
 import routes from '../client/routes';
 import { fetchComponentData } from './util/fetchData';
+import lanes from './routes/lane.routes';
+import notes from './routes/note.routes';
 import posts from './routes/post.routes';
 import dummyData from './dummyData';
 import serverConfig from './config';
@@ -70,6 +73,8 @@ app.use(compression());
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use(Express.static(path.resolve(__dirname, '../dist/client')));
+app.use('/api', lanes);
+app.use('/api', notes);
 app.use('/api', posts);
 
 // Render Initial HTML
@@ -89,7 +94,6 @@ const renderFullPage = (html, initialState) => {
         ${head.meta.toString()}
         ${head.link.toString()}
         ${head.script.toString()}
-
         ${isProdMode ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
         <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
