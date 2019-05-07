b4fa0adc36c68a3cf2f826223a68858548e55847
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-05-08 00:39:53 +0200

    fix routes

--- server/models/note.js
+++ server/models/note.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
+const mon = require('mongoose');
+mon.plugin(schema => { schema.options.usePushEach = true });
+
 const noteSchema = new Schema({
   task: { type: 'String', required: true },
-  id: { type: 'String', required: true, unique: true }
+  id: { type: 'String', required: true, unique: true },
 });
 
 export default mongoose.model('Note', noteSchema);
--- server/routes/note.routes.js
+++ server/routes/note.routes.js
@@ -5,5 +5,7 @@ const router = new Router();
 
 // Add a new Note
 router.route('/notes').post(NoteController.addNote);
+router.route('/notes:noteId').put(NoteController.editNote);
+router.route('/notes:noteId').delete(NoteController.deleteNote);
 
 export default router;
