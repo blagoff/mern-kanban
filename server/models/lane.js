6910fb1e8b35855445aed7e4cccc6eeaf969f0fb
Przemyslaw Przewoznik <blagoff88@gmail.com>
2019-05-08 21:04:40 +0200

    fix lane model

--- server/models/lane.js
+++ server/models/lane.js
@@ -5,7 +5,7 @@ const laneSchema = new Schema({
   name: { type: 'String', required: true },
   notes: [{ type: Schema.ObjectId, ref: 'Note', required: true }],
   id: { type: 'String', required: true, unique: true },
-});
+}, { usePushEach: true });
 
 laneSchema.pre('find', function (next) {
   this.populate('notes');
